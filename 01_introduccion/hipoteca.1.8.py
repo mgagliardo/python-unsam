7e0459167712cd547396c435d4d1fc85d1d94b6d
Miguel Angel Gagliardo <miguel.gagliardo@n26.com>
2021-08-09 12:22:48 +0200

    Adding 1.8 1.9

--- 01_introduccion/hipoteca.1.8.py
+++ 01_introduccion/hipoteca.1.8.py
@@ -2,14 +2,15 @@ saldo = 500000.0
 tasa = 0.05
 pago_mensual = 2684.11
 total_pagado = 0.0
+pago_extra = 1000
 mes = 0
 
 while saldo > 0:
-    saldo = saldo * (1+tasa/12) - pago_mensual
+    saldo = saldo * ( 1 + tasa / 12 ) - pago_mensual
     total_pagado = total_pagado + pago_mensual
     if mes < 12:
-      saldo = saldo - 1000
-      total_pagado = total_pagado + 1000
+      saldo = saldo - pago_extra
+      total_pagado = total_pagado + pago_extra
     mes = mes + 1
     print("Total pagado al mes {0}: {1}".format(mes, round(total_pagado, 2)))
 
---
+++ 01_introduccion/hipoteca.1.9.py
@@ -0,0 +1,19 @@
+saldo = 500000.0
+tasa = 0.05
+pago_mensual = 2684.11
+total_pagado = 0.0
+pago_extra_mes_comienzo = 61
+pago_extra_mes_fin = 108
+pago_extra = 1000
+mes = 0
+
+while saldo > 0:
+    saldo = saldo * ( 1 + tasa / 12 ) - pago_mensual
+    total_pagado = total_pagado + pago_mensual
+    if mes >= pago_extra_mes_comienzo and mes <= pago_extra_mes_fin:
+      saldo = saldo - pago_extra
+      total_pagado = total_pagado + pago_extra
+    mes = mes + 1
+    print("Total pagado al mes {0}: {1}".format(mes, round(total_pagado, 2)))
+
+print('Total pagado', round(total_pagado, 2))
