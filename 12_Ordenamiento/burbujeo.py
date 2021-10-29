d4f6e3d4c114e641d58a591c09e4b5d0ecc1b0a1
Miguel Gagliardo <miguel.gagliardo@signal-ai.com>
2021-10-29 20:34:39 +0100

    Burbujeo, entrega12

--- 12_Ordenamiento/burbujeo.py
+++ 12_Ordenamiento/burbujeo.py
@@ -8,4 +8,11 @@ def ord_burbujeo(lista):
     return lista
 
 def ord_burbujeo_recursivo(lista):
-    
+    if len(lista)
+    for i in range(len(lista) - 1):
+        for j in range(len(lista) - 1 - i):
+            if lista[j] > lista[j+1]:
+                tmp = lista[j]
+                lista[j] = lista[j+1]
+                lista[j+1] = tmp
+    return lista
