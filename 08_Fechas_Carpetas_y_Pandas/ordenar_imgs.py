39edcf018d1954c7518079d8d4de6c035a13e932
Miguel Gagliardo <miguel.gagliardo@signal-ai.com>
2021-11-02 21:38:34 +0000

    Agregando ordenar_imgs

--- 08_Fechas_Carpetas_y_Pandas/ordenar_imgs.py
+++ 08_Fechas_Carpetas_y_Pandas/ordenar_imgs.py
@@ -2,47 +2,57 @@ import os
 import sys
 from datetime import datetime
 
-def archivos_png(directorio):
+glob_carpeta_nueva = '../Data/imgs_procesadas'
+
+def borrar_vacias(ruta):
+    print("Borrando carpetas vacias..")
+    folders = list(os.walk(ruta))[1:]
+    for folder in folders:
+        nom_carpeta = folder[0]
+        contenido = folder[2]
+        if not contenido:
+            print(f"Borrando {nom_carpeta} ..")
+            os.rmdir(folder[0])
+
+def get_archivos_png(directorio):
     archivos = []
     for root, _, files in os.walk(directorio):
         if files:
-            archivos += [os.path.join(root, fname) for fname in files if '.png' in fname] # Split para obtener solo el nombre del file, sin full path.
+            archivos += [os.path.join(root, fname) for fname in files if '.png' in fname]
     return archivos
 
 def procesar_nombre(fname):
     fecha = fname.split('_')[-1].split('.')[0]
-    fecha_mod = datetime(year = int(fecha[0:4]), month = int(fecha[4:6]), day = int(fecha[6:8]))
-    fname = fname.replace(fname.split('/')[-1], fname.split('/')[-1].split('_')[0] + '.png')
+    fecha_mod = datetime(year = int(fecha[0:4]), month = int(fecha[4:6]), day = int(fecha[6:8])).timestamp()
+    fname = glob_carpeta_nueva + "/" + fname.replace(fname.split('/')[-1], fname.split('/')[-1].split('_')[0] + '.png').split('/')[-1]
     return fecha_mod, fname
 
 def procesar(fname):
-    for archivo in fname:
-        fecha_mod, name = procesar_nombre(archivo)
-        ts_mod = fecha_mod.timestamp()
-        os.utime(name, (ts_mod, ts_mod))
-        os.rename(fname, name)
-        
-
-def crear_carpeta(ruta, carpeta):
     try:
-        os.mkdir(os.path.join(ruta, carpeta))
-        print(f"Mostrando directorio {ruta}")
-    except FileExistsError:
-        print(f"El subdirectorio {carpeta} ya existe!")
+        fecha_mod, name = procesar_nombre(fname)
+        print(f"Renombrando Archivo {fname} a {name}")
+        os.rename(fname, name)
+        os.utime(name, (fecha_mod, fecha_mod))
+    except FileNotFoundError:
+        print(f'No se encuentra el directorio {fname}')
 
-def ordenar(ruta):
-    carpeta_nueva = 'imgs_procesadas'
-    crear_carpeta(ruta, carpeta_nueva)
-    archivos = archivos_png(ruta)
-    print(archivos)
-    procesar(archivos)
+def crear_carpeta(carpeta):
+    try:
+        os.mkdir(carpeta)
+        print(f"Directorio {carpeta} creado.")
+    except FileExistsError:
+        print(f"El subdirectorio {carpeta} ya existe. No hace falta crear.")
+
+def ordenar():
+    ruta = '../Data/ordenar'
+    archivos = get_archivos_png(ruta)
+    if archivos:
+        crear_carpeta(glob_carpeta_nueva)
+        for archivo in archivos:
+            procesar(archivo)
+    else:
+        print(f"No hay archivos .png en el directorio {ruta}")
+    borrar_vacias(ruta)
 
 if __name__ == "__main__":
-    try:
-        if len(sys.argv) == 2:
-            ruta = sys.argv[1]
-        else:
-            ruta = '../Data/ordenar'
-        ordenar(ruta)
-    except FileNotFoundError:
-        print(f'No se encuentra el directorio {sys.argv}')
+    ordenar()
